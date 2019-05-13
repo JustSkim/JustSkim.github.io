e6433c046ecad109a0e6799aa84e87f11df36a7a
JustSkim <1424266960@qq.com>
2019-05-13 21:03:42 +0800

    you can slide the sidabar now
    增加了滑动功能

--- js/main.js
+++ js/main.js
@@ -5,18 +5,25 @@ var sidabar = 1;
 
 //点击“收起”按钮收起侧边菜单
 $(".sidabar>button").click(function(){
-    $(".sidabar").hide();
+    $(".sidabar").animate({
+        left:"-300px"
+    },"slow");
     sidabar=1;
 });
 
 //点击按钮即可收起也可展开侧边栏
 $(".main>.sidabarButton").click(function(){
     if(sidabar||sidabar.hidden=='true'){
-        $(".sidabar").css("display","inline-block");
+        $(".sidabar").animate({
+            left:"0"
+        },"slow");
         sidabar=0;
     }
     else{
-        $(".sidabar").hide();
+        $(".sidabar").animate({
+            left:"-300px"
+        },"slow");
+        sidabar=1;
     }
 });
 

e5dc355d4fab5e98f6082891085058f38c4d1ed5
JustSkim <1424266960@qq.com>
2019-05-13 11:57:21 +0800

    Not origin text now
    改掉了原有样式的文字，添加了一些说明，侧边框初始不显现

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
-var sidabar = 0; 
+var sidabar = 1; 
+
 
 //点击“收起”按钮收起侧边菜单
 $(".sidabar>button").click(function(){
@@ -10,8 +11,8 @@ $(".sidabar>button").click(function(){
 
 //点击按钮即可收起也可展开侧边栏
 $(".main>.sidabarButton").click(function(){
-    if(sidabar){
-        $(".sidabar").show();
+    if(sidabar||sidabar.hidden=='true'){
+        $(".sidabar").css("display","inline-block");
         sidabar=0;
     }
     else{

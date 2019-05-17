0188c9facda4726e7ff142530b5c732ceb07821b
JustSkim <1424266960@qq.com>
2019-05-17 16:53:26 +0800

    Making by two components
    将侧边栏作为组件形式拆出，并优化按钮

--- js/main.js
+++ js/main.js
@@ -1,31 +1,35 @@
+var sidabar = document.getElementsByClassName("sidabar")[0];
+sidabar.innerHTML='<object type="text/html" data="components/sidabar.html" width="100%" height="100%"></object>';
+
 $(document).ready(function(){
 
-var sidabar = 1; 
-
-
-//点击“收起”按钮收起侧边菜单
-$(".sidabar>button").click(function(){
-    $(".sidabar").animate({
-        left:"-300px"
-    },"slow");
-    sidabar=1;
-});
 
 //点击按钮即可收起也可展开侧边栏
 $(".main>.sidabarButton").click(function(){
     if(sidabar||sidabar.hidden=='true'){
+        
         $(".sidabar").animate({
             left:"0"
         },"slow");
+        setTimeout(function(){
+            $(".main>.sidabarButton").removeClass("sidabar-fold");
+            $(".main>.sidabarButton>span").text("《");
+            $(".main>.sidabarButton").addClass("sidabar-unfold");
+        },1500);  
         sidabar=0;
     }
     else{
         $(".sidabar").animate({
             left:"-300px"
         },"slow");
+        $(".main>.sidabarButton>span").text("》");
+        $(".main>.sidabarButton").addClass("sidabar-fold");
+        $(".main>.sidabarButton").removeClass("sidabar-unfold"); 
         sidabar=1;
     }
 });
 
 
 });
+
+
